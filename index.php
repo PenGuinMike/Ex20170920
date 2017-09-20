5c05691917cc3b766148b87b447e897255bc85c1
PenGuinMike <104021074@live.asia.edu.tw>
2017-09-20 11:40:43 +0800

    save

--- index.php
+++ index.php
@@ -6,9 +6,36 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Mytitle</title>
-</head>
-<body>
+    <style>
 
+        .st1{
+            width="25%"
+        }
+        .st2{
+
+        }
+    </style>
+</head>
+<body bgcolor="#ffe4c4">
+
+    <div align="center" style=" line-height: 5px;">
+        <h1><p style="font-weight: bold">hello </p></h1>
+        <h2><p style="font-weight: bold">Aisa Uni</p></h2>
+        <h3><del><span style="color: #ff983d;font-size: 3em">A</span><span style="color: cornflowerblue;font-size: 4em">s</span><span style="color: darksalmon;font-size: 3em">i</span><span style="color: dodgerblue;font-size: 4em">a</span></del></h3>
+
+    </div>
+    <div align="center">
+        <table width="300" align="center">
+            <tr>
+                <td class="st1"><a href="index.php">首頁</a></td>
+                <td class="st1"><a href="page1.php">興趣</a></td>
+                <td class="st1"><a href="page2.php">超連結</a></td>
+                <td class="st1"><a href="page3.php">常用連結</a></td>
+            </tr>
+        </table>
+    </div>
+
+    <p align="center">power by penguin</p>
 </body>
 </html>
 <?php
