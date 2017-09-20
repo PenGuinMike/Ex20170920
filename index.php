176ddfb62b520ae59389718eb328b487fb8bec4a
PenGuinMike <104021074@live.asia.edu.tw>
2017-09-20 10:39:07 +0800

    save

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mytitle</title>
+</head>
+<body>
+
+</body>
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: USER
+ * Date: 2017/9/20
+ * Time: 上午 10:32
+ */
